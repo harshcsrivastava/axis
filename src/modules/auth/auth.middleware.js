9189c7817b51265c9e64b3b619608423ea8215b8
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-04-14 22:59:25 +0530

    feat: Add authentication middleware to verify access tokens and manage user sessions

---
+++ src/modules/auth/auth.middleware.js
@@ -0,0 +1,31 @@
+import { pool } from "../../../index.mjs";
+import ApiError from "../../utils/api-error.js";
+import { verifyAccessToken } from "../../utils/jwt-utils.js";
+
+const authenticate = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization?.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token){
+        return res.redirect("/api/auth/login")
+    }
+
+    const decoded = verifyAccessToken(token); // id & email from accessToken
+    const user = await pool.query("Select * from users where id = $1", [
+        decoded.id,
+    ]);
+    if (!user) throw ApiError.unauthorized("Not enough clearance!");
+    const safeUser = user.rows[0];
+
+    req.user = {
+        id: safeUser.id,
+        name: safeUser.name,
+        email: safeUser.email,
+    };
+
+    next();
+};
+
+export { authenticate };
